4edb3b5c076cd467e5010258e8d1995a0504e00e
SaharMFR <sweetysahora@gmail.com>
2023-09-21 16:29:20 -0700

    update in task-13

--- rotl.c
+++ rotl.c
@@ -7,6 +7,21 @@
  */
 void f_rotl(stack_t **head, unsigned int current_line)
 {
-	qaddnode(head, (*head)->n);
-	f_pop(head, current_line);
+	stack_t *temp1, *temp2;
+
+	if ((*head) && ((*head)->next))
+	{
+		temp1 = (*head)->next;
+		temp1->prev = NULL;
+
+		temp2 = *head;
+
+		while (temp2->next)
+			temp2 = temp2->next;
+
+		temp2->next = *head;
+		(*head)->next = NULL;
+		(*head)->prev = temp2;
+		*head = temp1;
+	}
 }
